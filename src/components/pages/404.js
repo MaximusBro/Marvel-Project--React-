d9d1e5ef543a9868c139476869900e3159446473
maximusbro <shidlovskiy.maksimka@gmail.com>
2022-12-19 00:11:51 +0100

    Helmet SEO optimization

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,8 +1,13 @@
+import { Helmet } from 'react-helmet';
 import ErrorMessage from "../errorMessage/ErrorMessage"
 import { Link } from "react-router-dom"
 const Page404 = () => {
 	return (
 		<div>
+			<Helmet>
+				<meta name="description" content={`Error 404 - page not found`} />
+				<title>Page not found</title>
+			</Helmet>
 			<ErrorMessage />
 			<p style={{ "textAlign": "center", "fontWeight": "bold", "fontSize": "24px" }}>Page doesn't exist</p>
 			<Link style={{
